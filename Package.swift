311cf0c5926c95975d45b2aaa58631d34a5aa3be
aamir-core <sayed.aamir@corecognitics.com>
2026-06-19 16:04:38 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,21 @@
-// swift-tools-version: 5.9   
+// swift-tools-version: 5.9
   import PackageDescription
-
+  
   let package = Package(
       name: "NexivoWidget",
-      platforms: [.iOS(.v15)],
+      platforms: [.iOS(.v15)],                    
       products: [
           .library(name: "NexivoWidget", targets: ["NexivoWidget"])
       ],
-      dependencies: [
-          .package(
-              url: "https://github.com/livekit/client-sdk-swift.git", from: "2.5.0")
-              checksum:"51cf80ef95f22c90545dec3a533e0ee780cdc0351bb36a7f6e997f44716ee2a9"
+      dependencies: [ 
+          .package(url:
+  "https://github.com/livekit/client-sdk-swift.git", from: "2.5.0")
+      ],    
+      targets: [
+          .binaryTarget(
+              name: "NexivoWidgetCore",           
+              url: "https://github.com/aamir-core/nexivo-ios-sdk-releases/releases/download/v1.0.5/NexivoWidget.xcframework.zip",
+              checksum: "51cf80ef95f22c90545dec3a533e0ee780cdc0351bb36a7f6e997f44716ee2a9"
           ),
           .target(
               name: "NexivoWidget",
@@ -18,7 +23,7 @@
                   "NexivoWidgetCore",
                   .product(name: "LiveKit", package: "client-sdk-swift")
               ],
-              path: "Sources/NexivoWidget"
+              path: "Sources/NexivoWidget"        
           )
       ]
   )
