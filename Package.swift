7629185968cc9c7ebee59f1cb93433b53b6b10a6
aamir-core <sayed.aamir@corecognitics.com>
2026-06-22 17:44:10 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@
       targets: [
           .binaryTarget(
               name: "NexivoWidgetCore",
-              url: "https://github.com/aamir-core/nexivo-ios-sdk-releases/releases/download/v1.0.7/NexivoWidget.xcframework.zip",
-              checksum: "08e7f34a67d91185dcfa01544c59a4f7f3550f2d2e608a9966b2508b214b9587"
+              url: "https://github.com/aamir-core/nexivo-ios-sdk-releases/releases/download/v1.0.8/NexivoWidgetSDK.xcframework.zip",
+              checksum: "d05da14561649e4f54b12f8ca9679cac1a7392cb35852df8c432da0c894dfe08"
           )
       ]
   )
